7956bf8e0085cd3e29cbb1b2c3d488fc7db2e62a
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-10 12:57:02 +0530

    Update csvLoader.js

--- js/data/csvLoader.js
+++ js/data/csvLoader.js
@@ -9,7 +9,7 @@ const headers = rows.shift()
 
 return rows.map(r => {
 
-let obj = {}
+const obj = {}
 
 headers.forEach((h,i)=>{
 
@@ -23,15 +23,27 @@ return obj
 
 }
 
-export async function loadAllSheets(){
 
-for(const key in SHEET_URLS){
+
+export async function loadAllSheets(updateProgress){
+
+const keys = Object.keys(SHEET_URLS)
+
+for(let i=0;i<keys.length;i++){
+
+const key = keys[i]
 
 const res = await fetch(SHEET_URLS[key])
 
 const txt = await res.text()
 
 dataStore[key] = parseCSV(txt)
+
+
+
+const percent = Math.round(((i+1)/keys.length)*100)
+
+updateProgress(percent)
 
 }
 
