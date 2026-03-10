0ecfcb92a42e143256072edd220dd438f703c78b
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-10 11:46:28 +0530

    Update csvLoader.js

--- js/data/csvLoader.js
+++ js/data/csvLoader.js
@@ -1,1 +1,38 @@
+import { SHEET_URLS } from "../config/sheetConfig.js"
+import { dataStore } from "./dataStore.js"
 
+function parseCSV(text){
+
+const rows = text.split("\n").map(r => r.split(","))
+
+const headers = rows.shift()
+
+return rows.map(r => {
+
+let obj = {}
+
+headers.forEach((h,i)=>{
+
+obj[h.trim()] = r[i]
+
+})
+
+return obj
+
+})
+
+}
+
+export async function loadAllSheets(){
+
+for(const key in SHEET_URLS){
+
+const res = await fetch(SHEET_URLS[key])
+
+const txt = await res.text()
+
+dataStore[key] = parseCSV(txt)
+
+}
+
+}
