ac6fa96e3d25855aeeb404d641d2b3b1c768014c
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-10 13:30:29 +0530

    Update filterEngine.js

--- js/core/filterEngine.js
+++ js/core/filterEngine.js
@@ -1,1 +1,30 @@
+export function filterData(data, filters){
 
+return data.filter(row =>{
+
+if(filters.acc && row["ACC"] !== filters.acc)
+return false
+
+if(filters.startDate){
+
+const date = new Date(row["Date"])
+
+if(date < new Date(filters.startDate))
+return false
+
+}
+
+if(filters.endDate){
+
+const date = new Date(row["Date"])
+
+if(date > new Date(filters.endDate))
+return false
+
+}
+
+return true
+
+})
+
+}
