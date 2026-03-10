a901ef9303c5780406c390ab9c1f18c1702d2992
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-10 14:15:26 +0530

    Update csvLoader.js

--- js/data/csvLoader.js
+++ js/data/csvLoader.js
@@ -1,19 +1,25 @@
-import { SHEET_URLS } from "../config/sheetConfig.js"
 import { dataStore } from "./dataStore.js"
+import { sheetUrls } from "../config/sheetConfig.js"
 
-function parseCSV(text){
+
+
+async function loadSheet(key, url){
+
+const res = await fetch(url)
+
+const text = await res.text()
 
 const rows = text.split("\n").map(r => r.split(","))
 
 const headers = rows.shift()
 
-return rows.map(r => {
+const data = rows.map(row=>{
 
 const obj = {}
 
 headers.forEach((h,i)=>{
 
-obj[h.trim()] = r[i]
+obj[h.trim()] = row[i]
 
 })
 
@@ -21,30 +27,34 @@ return obj
 
 })
 
+dataStore[key] = data
+
 }
 
 
 
 export async function loadAllSheets(updateProgress){
 
-const keys = Object.keys(SHEET_URLS)
+const keys = Object.keys(sheetUrls)
 
-for(let i=0;i<keys.length;i++){
-
-const key = keys[i]
-
-const res = await fetch(SHEET_URLS[key])
-
-const txt = await res.text()
-
-dataStore[key] = parseCSV(txt)
+let loaded = 0
 
 
 
-const percent = Math.round(((i+1)/keys.length)*100)
+await Promise.all(
+
+keys.map(async key => {
+
+await loadSheet(key, sheetUrls[key])
+
+loaded++
+
+const percent = Math.round((loaded/keys.length)*100)
 
 updateProgress(percent)
 
-}
+})
+
+)
 
 }
