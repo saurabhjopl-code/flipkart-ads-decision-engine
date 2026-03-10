4bc0fc2e480b6436ee5d62d9c14966beb2f2529b
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-10 15:11:17 +0530

    Update decisionRenderer.js

--- js/renderers/decisionRenderer.js
+++ js/renderers/decisionRenderer.js
@@ -1,18 +1,8 @@
 export function renderDecisions(decisions){
 
-let urgent = 0
-let growth = 0
-let optimize = 0
-
-decisions.forEach(d=>{
-
-if(d.priority === "High") urgent++
-
-else if(d.recommendation.includes("Increase")) growth++
-
-else optimize++
-
-})
+const urgent = decisions.filter(d=>d.priority==="High").length
+const growth = decisions.filter(d=>d.recommendation.includes("Increase")).length
+const optimize = decisions.filter(d=>d.priority==="Medium").length
 
 document.getElementById("urgent-count").innerText = urgent
 document.getElementById("growth-count").innerText = growth
