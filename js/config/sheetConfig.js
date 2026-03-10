c6597f348d0fcf0479d95139cf72f9db34eafbfd
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-10 11:45:59 +0530

    Update sheetConfig.js

--- js/config/sheetConfig.js
+++ js/config/sheetConfig.js
@@ -1,1 +1,11 @@
+export const SHEET_URLS = {
 
+CDR: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTd_uFyNN_LsA0hZ4EuRYflMnzY-NwSAn9sRhvPbbeRrRkAe5d07tIEJ_gilGwvR5-H1l3jjTOdjq6j/pub?gid=2107027254&single=true&output=csv",
+
+CFR: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTd_uFyNN_LsA0hZ4EuRYflMnzY-NwSAn9sRhvPbbeRrRkAe5d07tIEJ_gilGwvR5-H1l3jjTOdjq6j/pub?gid=1866392444&single=true&output=csv",
+
+PPR: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTd_uFyNN_LsA0hZ4EuRYflMnzY-NwSAn9sRhvPbbeRrRkAe5d07tIEJ_gilGwvR5-H1l3jjTOdjq6j/pub?gid=1507610887&single=true&output=csv",
+
+CKR: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTd_uFyNN_LsA0hZ4EuRYflMnzY-NwSAn9sRhvPbbeRrRkAe5d07tIEJ_gilGwvR5-H1l3jjTOdjq6j/pub?gid=599919115&single=true&output=csv"
+
+}
